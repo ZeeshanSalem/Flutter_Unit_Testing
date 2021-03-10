9c99a8fd4f17fb7ac53b96dbbf2ec805f2681c05
ZeeshanSalem <zeeshansaleem729@gmail.com>
2021-03-10 12:08:30 +0500

    counter test is done

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,13 +1,26 @@
-// Import the test package and Counter class
 import 'package:flutter_unit_testing/counter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
 
-    counter.increment();
+    test('value should be incremented', () {
+      final counter = Counter();
 
-    expect(counter.value, 1);
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
   });
 }
